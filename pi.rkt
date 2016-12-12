8e17621625124c8c519e06beec2a97696d061add
I2obiN <thomashood10@protonmail.com>
2016-12-12 23:08:28 +0000

    Create pi.rkt

---
+++ pi.rkt
@@ -0,0 +1,12 @@
+;;; R5RS
+(define (pi x y count)
+  (if (= (modulo count 2) 0)
+  (- y (/ 1 x)
+  (+ y 1)
+  (+ x 2)
+  (+ count 1))
+      ((+ y (/ 1 x))
+      (+ y 1)
+      (+ x 2)
+      (+ count 1)))
+  (pi x y count))
