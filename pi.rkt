57e17823fcb65ded2b1d5ab786c4c958e9f169b5
I2obiN <thomashood10@protonmail.com>
2016-12-17 18:55:00 +0000

    Update pi.rkt

--- pi.rkt
+++ pi.rkt
@@ -1,15 +1,17 @@
 ;;; R5RS
 ;;; Iterative-recursive pi procedure, probably needs a lot of work
-;;; (pi-get 1000)
 
 (define (pi-get n)
-  (pi-iter 0 1 n 0))
+  (pi 1 1 n 0))
 
-(define (pi-iter sum a n count)
-  (if (< count n)
-  (if (= (modulo count 2) 0) (pi-iter (+ sum (pi-calc a)) (+ a 2) n (+ count 1))
-    (pi-iter (- sum (pi-calc a)) (+ a 2) n (+ count 1)))) 
-  (display (* 4 sum)) (newline))
+(define (pi sum a n count)
+  (if (= n 0) 0)
+  (cond ((< count n)
+         (cond ((= (modulo count 2) 1)
+                (pi (+ sum (pi-calc (+ 2 a))) (+ a 2) n (+ count 1)))
+               ((= (modulo count 2) 0)
+                (pi (- sum (pi-calc (+ 2 a))) (+ a 2) n (+ count 1))))
+  (display (* 4 sum)) (newline))))
 
 (define (pi-calc a)
   (/ 1.0 a))
